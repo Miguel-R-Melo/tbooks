f7b70941d70c88d3e39009f0818d90a84be06878
Miguel <mmelo8176@gmail.com>
2022-02-06 10:26:41 -0300

    fonte adicionada

--- scripts/geral.js
+++ scripts/geral.js
@@ -4,3 +4,7 @@ document.getElementById('tl').addEventListener('click', cl)
 function cl(){
     window.alert('Você será redirecionado para um site de terceiros!')
 }
+
+function click(){
+    alert("Formulário enviado!")
+}
